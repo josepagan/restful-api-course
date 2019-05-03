1757bae2c5604e6877488cabc93db3c8a7b5d3ca
Jose Pagan <jm.pagan@gmail.com>
2019-05-03 10:52:25 +0100

    get and post working with postman

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   {id: 1, name: 'course1'},
   {id: 2, name: 'course2'},
@@ -23,12 +25,19 @@ const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send('The course with the given ID was not found');
   res.send(course);
 });
-app.post('api/courses', (req, res) => {
+app.post('/api/courses', (req, res) => {
   //we need to create a new course object to inject it in the post reques
   const course = {
-    
-  }
-})
+    id: courses.length + 1,
+    name: req.body.name 
+    // for req.body.name to work we need to enable parsing of json objets in body of req
+    // app.use(express.json())
+  };
+  courses.push(course);
+  //by convention it is always good to send what has chaged, the new course added.f
+  //
+  res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
